bfb82b02f349a64126e5c1e9efecd2067d2cd876
phamducdat <phamducdat2402@gmail.com>
2021-07-03 21:52:08 +0700

    add template email

--- Email/Original/spring-boot-email/src/main/java/com/infybuzz/app/EmailController.java
+++ Email/Original/spring-boot-email/src/main/java/com/infybuzz/app/EmailController.java
@@ -2,6 +2,7 @@ package com.infybuzz.app;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class EmailController {
 	@GetMapping("/sendEmailwithAttachment")
 	public String sendEmailwithAttachment() {
 		return emailService.sendEmailwithAttachment();
+	}
+
+	@PostMapping("/sendEmailWithTemplate")
+	public String sendEmailWithTemplate() {
+		return emailService.sendEmailWithTemplate();
 	}
 	
 }
--- Email/Original/spring-boot-email/src/main/java/com/infybuzz/app/EmailService.java
+++ Email/Original/spring-boot-email/src/main/java/com/infybuzz/app/EmailService.java
@@ -1,20 +1,32 @@
 package com.infybuzz.app;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.springframework.ui.Model;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 @Service
 public class EmailService {
 	
 	@Autowired
 	JavaMailSender javaMailSender;
+
+	@Autowired
+	private Configuration config;
 
 	public String sendEmail() {
 		SimpleMailMessage message = new SimpleMailMessage();
@@ -52,5 +64,38 @@ public class EmailService {
 		} catch (Exception e) {
 			return "Mail sent failed";
 		}
+	}
+
+	public String sendEmailWithTemplate() {
+		try {
+			MimeMessage message = javaMailSender.createMimeMessage();
+
+			MimeMessageHelper messageHelper =
+					new MimeMessageHelper(message, true);
+			//model có thể đặt bằng null nếu như không có thuộc tính nào cần thay thế
+			Map<String, Object> model = new HashMap<>();
+
+			model.put("Name", "Lê Tuấn Anh");
+			model.put("Username", "tuanlatuan");
+			//Qua trinh chuyen doi template sang string
+			Template t = config.getTemplate("email-template.html");
+			String html = FreeMarkerTemplateUtils.processTemplateIntoString(t,model);
+			//Thay the logo
+			FileSystemResource resource = new FileSystemResource(new File("D:\\temp\\logo.png"));
+			messageHelper.setFrom("phamducdat2402@gmail.com");
+			messageHelper.setTo("phamducdat2402@gmail.com");
+			messageHelper.setSubject("Test Subject");
+			messageHelper.setText(html, true);
+			//Bắt buộc addInline phải sau setText, sẽ thay thế contentld bằng file,bla,bla,... tuy nhiên
+			//không thay được bằng mỗi string
+			messageHelper.addInline("image001", resource);
+			javaMailSender.send(message);
+
+			return "Mail sent successfully";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Failed";
+		}
+
 	}
 }
